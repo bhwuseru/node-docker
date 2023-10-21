b0534aa2581cb3603398e86d37abae6b5976f1f6
Takurou Nakada <takurou403@gmail.com>
2023-10-21 10:28:14 +0900

    node Dockerfileにsudoとbashを導入/立ち上げスクリプト調整

--- bin/reset_docker_environment.sh
+++ bin/reset_docker_environment.sh
@@ -3,23 +3,23 @@
 # docker composeで構築した環境を初期状態に戻す
 # 注意： 構築したデータベースは削除される
 
-# 1. .envrcを読み込み、プロジェクトディレクトリに移動
+# .envrcを読み込み、プロジェクトディレクトリに移動
 source .envrc && \
 cd .${PROJECT_NAME} && \
 
-# 2. Docker Composeを使って現在の環境を停止
+#  Docker Composeを使って現在の環境を停止
 docker-compose down && \
 
-# 3. dangling（未使用の）Dockerイメージがあるかをチェック
+# dangling（未使用の）Dockerイメージがあるかをチェック
 none_images=$(docker images -f "dangling=true" -q)
 if [ -n "$none_images" ]; then
     # 4. danglingイメージを削除
     docker rmi $(docker images -f "dangling=true" -q)
 fi
 
-# 5. データディレクトリ、初期化SQLファイル、.envファイルを削除
+# データディレクトリ、初期化SQLファイル、.envファイルを削除
 rm -rf './db/data' && rm -f './db/init/init.sql' && rm -f ".env" && \
 
-# 6. プロジェクトディレクトリを元に戻す
+# プロジェクトディレクトリを元に戻す
 cd ../ && \
 mv .${PROJECT_NAME} '.devcontainer'
--- bin/setup_docker_environment.sh
+++ bin/setup_docker_environment.sh
@@ -2,28 +2,32 @@
 
 # docker-compose環境のセットアップとコンテナの起動する
 
-# 1. .env ファイルのパスを設定
+# .env ファイルのパスを設定
 envfile_path="$(pwd)/.devcontainer/.env"
 
-# 2. もし .env ファイルが存在する場合は削除
+# もし .env ファイルが存在する場合は削除
 if [ -f $envfile_path ]; then
     rm -f $envfile_path
 fi
 
-# 3. Docker Composeの環境設定を生成および実行
-. ./bin/gene_docker_compose_env.sh && \
+#  .devcontainerディレクトリ(初期状態の場合)に限り.envrcの準備処理を実行
+if [ -d "$(pwd)/.devcontainer" ]; then
+    # Docker Composeの環境設定を生成および実行
+    . ./bin/gene_docker_compose_env.sh
+fi
+
 source .envrc && \
 cd .${PROJECT_NAME} 
 
-# 4. dangling Dockerイメージの削除
+# dangling Dockerイメージの削除
 none_images=$(docker images -f "dangling=true" -q)
 if [ -n "$none_images" ]; then
     docker rmi $(docker images -f "dangling=true" -q)
 fi
 
-# 5. Docker Composeを使用してコンテナをビルドおよび起動
+# Docker Composeを使用してコンテナをビルドおよび起動
 docker-compose build --no-cache && \
 docker-compose up -d && \
 
-# 6. 実行したファイルのディレクトリ位置に戻る
+# 実行したファイルのディレクトリ位置に戻る
 cd ..
